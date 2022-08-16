4284f036f9927b5c7abcf23e6b33c723d66623c4
ashesLiu <2673192509@qq.com>
2022-08-16 14:24:48 +0800

    977 v1.0

--- array/977.go
+++ array/977.go
@@ -1,21 +1,39 @@
 package array
 
-func sortedSquares(nums []int) []int {
+func SortedSquares(nums []int) []int {
 	n := len(nums)
-
 	for i := 0; i < n; i++ {
 		nums[i] = nums[i] * nums[i]
 	}
 
-	// 冒泡
-	for i := 0; i < n; i++ {
-		for j := i + 1; j < n; j++ {
-			if nums[i] > nums[j] {
-				swap(nums, i, j)
-			}
+	// 快排
+	QuichSort(nums, 0, len(nums))
+	return nums
+}
+
+// quichSort: [left,right)
+func QuichSort(nums []int, left, right int) {
+	if left < right {
+		idx := partition(nums, left, right)
+		QuichSort(nums, left, idx)
+		QuichSort(nums, idx+1, right)
+	}
+}
+
+func partition(nums []int, l, r int) int {
+	// 选择nums[l]为枢轴元素
+	v := nums[l]
+	// 双指针: i遍历 pos为已遍历元素的划分点
+	// 循环不变量： nums[l:pos)<pivot nums[pos:i)>=pivot
+	pos := l
+	for i := l + 1; i < r; i++ {
+		if nums[i] < v {
+			swap(nums, pos, i)
+			pos++
 		}
 	}
-	return nums
+	// swap(nums, pos, l)
+	return pos
 }
 
 func swap(nums []int, a, b int) {
