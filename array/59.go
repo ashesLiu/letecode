b1c4e010bf49d63d6ae9ee71310f353a8403e627
ashesLiu <2673192509@qq.com>
2022-08-19 14:05:41 +0800

    59 v2.0

--- array/59.go
+++ array/59.go
@@ -1,12 +1,12 @@
 package array
 
-import "fmt"
-
 func generateMatrix(n int) [][]int {
 	if n == 0 {
 		return nil
 	}
 	direction := [][]int{{0, 1}, {1, 0}, {0, -1}, {-1, 0}}
+	// i:0,1 j:2,3
+	border := []int{0, n - 1, 0, n - 1}
 	pos, i, j := 0, 0, 0
 	nextPos := func(i int) int {
 		if i == 3 {
@@ -16,19 +16,25 @@ func generateMatrix(n int) [][]int {
 		}
 	}
 	arr := make([][]int, n)
-	vis := make([][]int, n)
 	for k := 0; k < n; k++ {
-		vis[k] = make([]int, n)
 		arr[k] = make([]int, n)
 	}
-	// fmt.Println("%v",arr)
 	for k := 1; k <= n*n; k++ {
-		fmt.Printf("%v %v\n", i, j)
+		// fmt.Printf("%v %v\n", i, j)
 		arr[i][j] = k
-		vis[i][j] = 1
 		tempi := i + direction[pos][0]
 		tempj := j + direction[pos][1]
-		if tempi >= n || tempi < 0 || tempj >= n || tempj < 0 || vis[tempi][tempj] == 1 {
+		if tempi < border[0] || tempi > border[1] || tempj < border[2] || tempj > border[3] {
+			switch pos {
+			case 0:
+				border[0]++
+			case 1:
+				border[3]--
+			case 2:
+				border[1]--
+			case 3:
+				border[2]++
+			}
 			pos = nextPos(pos)
 		}
 		i += direction[pos][0]
