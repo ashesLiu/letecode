663ed58e997f9ca17bbfbadaa78599b3390c22c9
ashesLiu <2673192509@qq.com>
2022-08-19 11:29:07 +0800

    76 v2.0

--- array/76.go
+++ array/76.go
@@ -1,23 +1,34 @@
 package array
 
 func minWindow(s string, t string) string {
-	hashmap := make(map[byte]int)
-	window := make(map[byte]int)
+	need := make(map[byte]int)
 	cnt := 0
 	ans := ""
 	for i := 0; i < len(t); i++ {
-		hashmap[t[i]]++
-		cnt++
+		need[t[i]]++
 	}
-	i, left := 0, 0
-	for i < len(s) {
-		window[s[i]]++
-		i++
-		for check(hashmap, window) {
-			if ans == "" || len(ans) > (i-left) {
-				ans = s[left:i]
+	left := 0
+	for i := 0; i < len(s); i++ {
+		// cnt 记录覆盖到的有效字符数
+		if need[s[i]] > 0 {
+			cnt++
+		}
+		need[s[i]]--
+		// fmt.Printf("%v %v %v %v\n", left, i, string(s[left]), string(s[i]))
+		// fmt.Printf("%v\n", need)
+		if cnt == len(t) {
+			// left右移，移过多余字符
+			for need[s[left]] < 0 {
+				need[s[left]]++
+				left++
 			}
-			window[s[left]]--
+			// 更新ans
+			if len(ans) == 0 || len(ans) >= (i-left+1) {
+				ans = s[left : i+1]
+			}
+			// 收缩使不满足条件
+			cnt--
+			need[s[left]]++
 			left++
 		}
 	}
