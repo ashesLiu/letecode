3dcf2e9a39e77256349f84ae56a4e33101376b9c
ashesLiu <2673192509@qq.com>
2022-08-26 13:51:40 +0800

    15 final

--- hashTable/15.go
+++ hashTable/15.go
@@ -3,33 +3,35 @@ package hashTable
 func threeSum(nums []int) [][]int {
 	sort.Ints(nums)
 	ans := make([][]int,0)
+	twoSearch := func(target, left, right int){
+		l,r:=left,right
+		for l<r{
+			sum := nums[l]+nums[r]
+			if sum == target{
+				ans = append(ans, []int{-target, nums[l],nums[r]})
+				l++
+				r--
+			}else if sum < target {
+				l++
+			}else{
+				r--
+			}
+			for l<r && l>left && nums[l]==nums[l-1]{
+				l++
+			}
+			for l<r && r<right && nums[r] == nums[r+1]{
+				r--
+			}
+		}
+	}
 	for i:=0;i<len(nums);i++{
 		if i>0&&nums[i]==nums[i-1]{
 			continue
 		}
-		for j:=i+1;j<len(nums);j++{
-			if i>0&&nums[i]==nums[i-1]{
-				continue
-			}
-			if BinarySearch(nums,-nums[i]-nums[j],j+1,len(nums)-1){
-				ans = append(ans, []int{nums[i],nums[j],-nums[i]-nums[j]})	
-			}
+		if nums[i] > 0{
+			break
 		}
+		twoSearch(-nums[i],i+1,len(nums)-1)
 	}
 	return ans
-}
-
-func BinarySearch(nums []int,target,left,right int) bool{
-	l,r:=left,right
-	for l<=r{
-		mid := l + (r-l)/2
-		if nums[mid] == target{
-			return true
-		}else if nums[mid]<target{
-			l = mid+1
-		}else{
-			r = mid-1
-		}
-	}
-	return false
 }
