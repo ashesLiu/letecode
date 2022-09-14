4b0ce23b1afa11f6aad33853bd3f1a8f62804e07
ashesLiu <2673192509@qq.com>
2022-09-14 15:57:56 +0800

    151 final

--- charArray/151.go
+++ charArray/151.go
@@ -1,40 +1,44 @@
 package charArray
 
 func reverseWords(s string) string {
+	//reverse [left, right)
+	reverse := func(data []byte, left, right int) {
+		l, r := left, right-1
+		for l < r {
+			data[l], data[r] = data[r], data[l]
+			l++
+			r--
+		}
+	}
 	str := []byte(s)
 	l, r := 0, len(str)-1
-
+	// 去掉前后空格
 	for l < len(str) && str[l] == ' ' {
 		l++
 	}
-
 	for r >= 0 && str[r] == ' ' {
 		r--
 	}
+	low, fast := l, l
+	// 去掉中间多余空格
+	for fast <= r {
+		if !(str[fast] == ' ' && fast-1 > 0 && str[fast] == str[fast-1]) {
+			str[low] = str[fast]
+			low++
+		}
+		fast++
+	}
+	str = str[l:low]
+	// 反转整个str
+	reverse(str, 0, len(str))
 
-	lw := l
-	arr := make([]string,0)
-	for i := l; i <= r; i++ {
-		if str[i] == ' '{
-			temp := string(str[lw:i])
-			if temp != ""{
-                // fmt.Println(temp)
-				arr = append(arr, temp)
-			}
-			lw = i+1
+	// 反转单词
+	low, fast = 0, 0
+	for ;fast <= len(str);fast++ {
+		if fast == len(str)||str[fast] == ' ' {
+			reverse(str, low, fast)
+			low = fast + 1
 		}
 	}
-	temp := string(str[lw:r+1])
-	if temp != " "{
-        // fmt.Println(temp)
-		arr = append(arr, temp)
-	}	
-	var sb strings.Builder
-	for i:= len(arr)-1; i>=0;i--{
-		sb.WriteString(arr[i])
-		if i!=0 {
-			sb.WriteString(" ")
-		}
-	}
-	return sb.String()
+	return string(str)
 }
