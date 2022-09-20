4532fa2cd5e182b0ec373ab7a1610d73cd9861af
ashesLiu <2673192509@qq.com>
2022-09-20 11:12:18 +0800

    28 final

--- charArray/28.go
+++ charArray/28.go
@@ -8,12 +8,11 @@ import(
 func getNext(next []int, s string){
 	i:=0
 	for j:=1;j<len(s);j++{
-		if s[i] == s[j]{
+		for i>0 && s[i]!=s[j]{
+			i = next[i-1]
+		}
+		if s[i]==s[j]{
 			i++
-		}else{
-			for i>0 && s[i] != s[j]{
-				i = next[i-1]
-			}
 		}
 		next[j] = i
 	}
@@ -22,6 +21,17 @@ func getNext(next []int, s string){
 func strStr(haystack string, needle string) int {
 	next := make([]int, len(needle))
 	getNext(next,needle)
-	fmt.Println(next)
+	j:=0
+	for i:=0;i<len(haystack);i++{
+		for j>0 && haystack[i]!=needle[j]{
+			j = next[j-1]
+		}
+		if haystack[i]==needle[j]{
+			j++
+			if j==len(needle){
+				return i - j + 1
+			}
+		}
+	}
 	return -1
 }
